7fbadb933e3801c145642d441fb5a09772ed4548
osgarc05 <osgarc05@ucm.es>
2021-10-19 11:54:20 +0200

    Marp

---
+++ MARP/PoblemasMarp/Ej24/Source.cpp
@@ -0,0 +1,107 @@
+
+/*@ <answer>
+ *
+ * Nombre y Apellidos:Oscar García Castro
+ *
+ *@ </answer> */
+
+#include <iostream>
+#include <fstream>
+#include "GrafoValorado.h"
+#include "ConjuntosDisjuntos.h"
+#include "PriorityQueue.h"
+using namespace std;
+
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+template <typename Valor>
+class ARM_Kruskal {
+private:
+    std::vector<Arista<Valor>> _ARM;
+    Valor coste;
+    bool conexo;
+public:
+    Valor costeARM() const {
+        return coste;
+    }
+    std::vector<Arista<Valor>> const& ARM() const {
+        return _ARM;
+    }
+    bool isConexo() const { return conexo; };
+
+    ARM_Kruskal(GrafoValorado<Valor> const& g) : coste(0), conexo(false) {
+        PriorityQueue<Arista<Valor>> pq(g.aristas());
+        ConjuntosDisjuntos cjtos(g.V());
+        while (!pq.empty()) {
+            auto a = pq.top(); pq.pop();
+            int v = a.uno(), w = a.otro(v);
+            if (!cjtos.unidos(v, w)) {
+                cjtos.unir(v, w);
+                _ARM.push_back(a); coste += a.valor();
+                if (_ARM.size() == g.V() - 1) break;
+            }
+        }
+        if (cjtos.num_cjtos() == 1) conexo = true;
+    }
+};
+
+bool resuelveCaso() {
+
+    // leer los datos de la entrada
+    int x, y;
+    cin >> x;
+
+    if (!std::cin)  // fin de la entrada
+        return false;
+
+    GrafoValorado<int> g(x);
+    cin >> y;
+    for (int i = 0; i < y; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        Arista<int> z(--a, --b, c);
+        g.ponArista(z);
+    }
+    // resolver el caso posiblemente llamando a otras funciones
+
+    ARM_Kruskal<int> arm(g);
+    if (arm.isConexo()) cout << arm.costeARM() << "\n";
+    else cout << "No hay puentes suficientes\n";
+    // escribir la solución
+
+    return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
