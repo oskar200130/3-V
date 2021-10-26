af6c6ae6c63a1438d0a1eb8d4eb62060912217d3
osgarc05 <osgarc05@ucm.es>
2021-10-26 11:42:51 +0200

    POR FAVOOOOOOOOOOOOOOOOOOR

--- Fisica/skeleton/Particle.h
+++ Fisica/skeleton/Particle.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "core.hpp"
 #include "RenderUtils.hpp"
+#include <random>
+#include <math.h>
 
 using namespace physx;
+using namespace std;
 class Particle
 {
 private:
@@ -18,14 +21,76 @@ private:
 	RenderItem* renderItem;
 
 public:
-	Particle() :pos(0, 0, 0), vel(0), a(0), color({0,0,0,1}), damping(0), in_mas(0) { renderItem = nullptr; };
-	Particle(PxTransform p, Vector3 v, Vector3 ac, Vector4 c = { 1,1,1,1}, float dam = 0.999, float in_m = 5.0) :pos(p), vel(v), a(ac), color(c), damping(dam), in_mas(in_m) {
+	Particle() :pos(0, 0, 0), vel(0), a(0), color({ 0,0,0,1 }), damping(0), in_mas(0.1) { };
+	Particle(PxTransform p, Vector3 v, Vector3 ac, Vector4 c = { 1,1,1,1 }, float dam = 0.999, float in_m = 5.0) :pos(p), vel(v), a(ac), color(c), damping(dam), in_mas(in_m) {
 		renderItem = new RenderItem(CreateShape(PxSphereGeometry(2.0)), &pos, color);
 	};
+	void init(Vector3 p, Vector3 v, Vector3 ac, Vector4 c = { 1,1,1,1 }, float dam = 0.999, float in_m = 5.0, float size = 2.0);
 	~Particle();
-	inline float getY() const { return pos.p.y; };
-	void update(double t);
-	inline void setInactive() { active = false; };
+	void integrate(double t);
+
+	inline PxTransform getPosition() const { return pos; };
+	inline Vector3 getVel() const { return vel; };
 	inline bool isActive() const { return active; };
+
+	inline void setInactive() { active = false; };
 };
 
+//--------------------------------------------------------------------------------------------
+
+enum class FireWorkType { explosion, shoot, fireball };
+
+class FireWork : public Particle {
+public:
+	FireWorkType type;
+	float age;
+	int expAge = 0;
+
+	FireWork(FireWorkType t) : Particle(), type(t), age(0) {};
+	~FireWork() {};
+	bool update(float t);
+};
+
+//--------------------------------------------------------------------------------------------
+
+struct Payload {
+	FireWorkType type;
+	int count;
+
+	Payload(FireWorkType t, int c) {
+		type = t;
+		count = c;
+	};
+};
+
+//--------------------------------------------------------------------------------------------
+
+struct FireWorkRule {
+	FireWorkType type;
+
+	int minAge;
+	int maxAge;
+
+	float damping;
+
+	float numH = 0;
+
+	Vector3 minVelocity;
+	Vector3 maxVelocity;
+	Vector4 color;
+
+	std::vector<Payload> payloads;
+	void configType(FireWork* fir, Vector3& vel, float& size, Vector4& color);
+
+	void setParameters(FireWorkType t, float minA, float maxA, Vector3 minV, Vector3 maxV, float dam) {
+		type = t;
+		minAge = minA * 100;
+		maxAge = maxA * 100;
+		minVelocity = minV * 10;
+		maxVelocity = maxV * 10;
+		damping = dam;
+		color = { 1,1,1,1 };
+	};
+
+	void create(FireWork* fir, Vector3 pos, const FireWork* parent = NULL);
+};
