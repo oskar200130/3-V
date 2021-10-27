437a6b5a64c215037afc5c2d3d1d5e2aa6e512ab
oscar <osgarc05@ucm.es>
2021-10-27 19:39:24 +0200

    MARP

--- MARP/PoblemasMarp/Ej27/Source.cpp
+++ MARP/PoblemasMarp/Ej27/Source.cpp
@@ -26,27 +26,33 @@ using namespace std;
  // Escribe el código completo de tu solución aquí debajo
  // ================================================================
  //@ <answer>
+
 template <typename Valor>
-using Camino = std::deque<AristaDirigida<Valor>>;
+using Camino = deque<AristaDirigida<Valor>>;
 template <typename Valor>
 class Dijkstra {
 private:
-	const Valor INF = std::numeric_limits<Valor>::max();
+	const Valor INF = numeric_limits<Valor>::max();
 	int origen;
-	std::vector<Valor> dist;
-	std::vector<AristaDirigida<Valor>> ulti;
+	vector<pair<Valor, Valor>> dist;
+	vector<AristaDirigida<Valor>> ulti;
 	IndexPQ<Valor> pq;
 	void relajar(AristaDirigida<Valor> a) {
 		int v = a.desde(), w = a.hasta();
-		if (dist[w] > dist[v] + a.valor()) {
-			dist[w] = dist[v] + a.valor(); ulti[w] = a;
-			pq.update(w, dist[w]);
+		if (dist[w].second > dist[v].second + a.valor()) {
+			dist[w].second = dist[v].second + a.valor();
+			ulti[w] = a;
+			pq.update(w, dist[w].second);
+			dist[w].first = dist[v].first;
 		}
+		else if (dist[w].second == dist[v].second + a.valor())
+			dist[w].first += dist[v].first;
 	}
 public:
 	Dijkstra(DigrafoValorado<Valor> const& g) : origen(0),
-		dist(g.V(), INF), ulti(g.V()), pq(g.V()) {
-		dist[origen] = 0;
+		dist(g.V(), std::make_pair(INF, INF)), ulti(g.V()), pq(g.V()) {
+		dist[origen].first = 1;
+		dist[origen].second = 0;
 		pq.push(origen, 0);
 		while (!pq.empty()) {
 			int v = pq.top().elem; pq.pop();
@@ -54,8 +60,9 @@ public:
 				relajar(a);
 		}
 	}
-	bool hayCamino(int v) const { return dist[v] != INF; }
-	Valor distancia(int v) const { return dist[v]; }
+	bool hayCamino(int v) const { return dist[v].first != INF; }
+	Valor distancia(int v) const { return dist[v].second; }
+	int caminos(int v) const { return dist[v].first; }
 	Camino<Valor> camino(int v) const {
 		Camino<Valor> cam;
 		// recuperamos el camino retrocediendo
@@ -68,19 +75,14 @@ public:
 };
 
 bool resuelveCaso() {
-	// leer los datos de la entrada
+
 	int n;
 	cin >> n;
 
-	if (n == 0)  // fin de la entrada
+	if (!cin)  // fin de la entrada
 		return false;
 
 	DigrafoValorado<int> g(n);
-	vector<int> carga(n);
-	// resolver el caso posiblemente llamando a otras funciones
-	for (int i = 0; i < n; i++) {
-		cin >> carga[i];
-	}
 
 	int m;
 	cin >> m;
@@ -88,16 +90,16 @@ bool resuelveCaso() {
 	for (int i = 0; i < m; i++) {
 		int x, y, z;
 		cin >> x >> y >> z;
-		g.ponArista(AristaDirigida<int>(x-1, y-1, z + carga[y-1]));
+		g.ponArista(AristaDirigida<int>(x - 1, y - 1, z));
+		g.ponArista(AristaDirigida<int>(y - 1, x - 1, z));
 	}
 	Dijkstra<int> d(g);
 	// escribir la solución
 
 	if (d.hayCamino(n - 1))
-		cout << d.distancia(n - 1) + carga[0] << "\n";
+		cout << d.caminos(n - 1) << "\n";
 	else
-		cout << "IMPOSIBLE\n";
-
+		cout << "0\n";
 	return true;
 }
 
