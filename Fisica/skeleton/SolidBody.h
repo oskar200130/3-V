68402af17595a977dccb4e46f77091ca502c8ee4
osgarc05 <osgarc05@ucm.es>
2021-11-26 10:32:16 +0100

    Mucha fisica

--- Fisica/skeleton/SolidBody.h
+++ Fisica/skeleton/SolidBody.h
@@ -2,8 +2,6 @@
 #include "core.hpp"
 #include "RenderUtils.hpp"
 
-using namespace physx;
-
 class SolidBody {
 public:
 	PxRigidDynamic* rigid = nullptr;
@@ -12,17 +10,37 @@ public:
 	RenderItem* renderItem = nullptr;
 	PxVec3 force;
 	PxVec3 torque;
+
+	Vector3 vel;
+	Vector3 a;
+	Vector4 color;
+	float damping;
+	float in_mas;
+
+	bool active = true;
 };
 
 //----------------------------------------------------------------------
 
-class BodySystem {
+class BodySystem{
 public:
-	BodySystem(PxPhysics* _gPhysics, PxScene* _gScene, PxTransform pSet,
-		float stepSet = 1.5, bool colorR = true, float lifeSet = 20, float sizeSet = 1,
-		int max = 10, Vector4 colorSet = { 1, 0, 0, 1.0 });
+	BodySystem(PxPhysics* _gPhysics, PxScene* _gScene, PxTransform pSet, float stepSet = 1.5, bool _colorR = true, float lifeSet = 20, float sizeSet = 1,
+		int max = 10, Vector4 colorSet = { 1, 0, 0, 1.0 }) : gPhysics(_gPhysics), gScene(_gScene), p(pSet), step(stepSet), colorR(_colorR), life(lifeSet),
+		size(sizeSet), maxParticles(max), color(colorSet) {};
 	~BodySystem();
 	void addBody();
 	void integrate(float t);
 	vector<SolidBody*> bodies;
+private:
+	PxPhysics* gPhysics;
+	PxScene* gScene;
+	PxTransform p;
+	Vector4 color;
+	bool colorR;
+	float step;
+	float size;
+	int maxParticles;
+	int numParticles;
+	float life;
+	float timeSinceAdding;
 };
